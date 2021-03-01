f0aa78719a1f764a1a49f6966a935deb20f30980
Andy Bingham <andy.bingham@agrantec.com>
2021-03-01 14:43:48 +0000

    Add close

--- wiegand.js
+++ wiegand.js
@@ -5,23 +5,23 @@ module.exports = function (RED) {
 	function WiegandNode(config) {
 		RED.nodes.createNode(this, config);
 		var node = this;
-		
+
 		w.begin({ d0: 5, d1: 6 });
 
 		w.on('ready', () => {
 			node.log('Ready');
-			node.status({fill:"green",shape:"ring",text:"Ready"});
+			node.status({ fill: "green", shape: "ring", text: "Ready" });
 		});
 
 		w.on('data', (data) => {
 			node.log(`Recvd ${data.length} bits`);
 			if (data.length < 34) {
-				node.status({fill:"Failed",shape:"ring",text:`Only ${data.length} bits`});
+				node.status({ fill: "Failed", shape: "triangle", text: `Only ${data.length} bits` });
 			}
 		});
 		w.on('reader', (id) => {
 			node.log(`Read ${id.toString(16)}`);
-			node.status({fill:"green",shape:"ring",text:`Read ${id.toString(16)}`});
+			node.status({ fill: "green", shape: "ring", text: `Read ${id.toString(16)}` });
 			var msg = {
 				topic: 'tag',
 				payload: {
@@ -30,6 +30,11 @@ module.exports = function (RED) {
 					oct: id.toString(8)
 				}
 			}
+		});
+
+		node.on('close',()=>{
+			node.log(`Closing node, stopping IRQs`);
+			w.stop();
 		});
 	}
 
