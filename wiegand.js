72c59174e54b2ea65a969957956c1b79168ebd1d
Andy Bingham <andy.bingham@agrantec.com>
2021-03-01 14:54:37 +0000

    fix instance issue

--- wiegand.js
+++ wiegand.js
@@ -6,9 +6,7 @@ module.exports = function (RED) {
 	function WiegandNode(config) {
 		RED.nodes.createNode(this, config);
 		var node = this;
-		if (!w) {
-			this.w = new Wiegand();
-		}
+		node.w = (node.w) ? node.w : new Wiegand();
 		
 		w.begin({ d0: 5, d1: 6 });
 
